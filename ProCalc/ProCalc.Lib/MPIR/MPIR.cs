b564d59bfa29356877cf284a1ca4acfb8a2ea4b3
Ivan Neeson <zzymyn@gmail.com>
2017-11-17 17:24:42 +1100

    Start integrating with mpir.

---
+++ ProCalc/ProCalc.Lib/MPIR/MPIR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProCalc.Lib.MPIR
+{
+    public static class MPIR
+    {
+        public const int MPZSize = 128;
+
+        [DllImport("mpir.dll", EntryPoint = "__gmpz_init")]
+        public static extern void mpz_init(IntPtr a);
+
+        [DllImport("mpir.dll", EntryPoint = "__gmpz_init")]
+        public static extern void mpz_clear(IntPtr a);
+    }
+}
---
+++ ProCalc/ProCalc.Tests/MPIR_Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProCalc.Lib.MPIR;
+using System.Runtime.InteropServices;
+
+namespace ProCalc.Tests
+{
+    [TestClass]
+    public class MPIR_Tests
+    {
+        [TestMethod]
+        public void InitTest()
+        {
+            var a = Marshal.AllocHGlobal(MPIR.MPZSize);
+            MPIR.mpz_init(a);
+            MPIR.mpz_clear(a);
+        }
+    }
+}
